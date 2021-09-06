f06667ae9c1ae8ceca7c64b0ad95876d695702e3
FrederiqueK <frederiquekeijzer@gmail.com>
2021-09-06 11:20:46 +0000

    maandag

--- script.js
+++ script.js
@@ -3,13 +3,17 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+
+var x = 50;
+var y = 50;
+var speedX = 2;
+var speedY = 1;
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
-
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
 }
+
 
 
 /**
@@ -21,6 +25,22 @@ function draw() {
   // stel vulkleur in
   fill(100, 100, 255);
 
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('red');
+
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+
+  // positie van bal updaten
+  x = x + speedX;
+  y = y + speedY;
+
+  if (y === 680) {
+    speedY = speedY * -1;
+  }
+
+  if (x === 1240) {
+    speedX = speedX * -1;
+  }
+
 }
