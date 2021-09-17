d5574f8e7a233586e9431ec9c08bf2e710253469
FrederiqueK <frederiquekeijzer@gmail.com>
2021-09-17 06:19:49 +0000

    17sept

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+// globale variabelen
 var ballen = [];
+
 
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
-
-var x = 50; 
-var y = 50; 
-var speedX = 5; 
-var speedY = 5; 
-
-
-
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
-  for(var i = 0; i < bal.length; i++) {
-  var randomX = random(50, 1230);
-  var randomY = random(50, 670);
-  var randomSpeedX = random(-5, 5);
-  var randomSpeedY = random(-5, 5);
-  }
-}
 
+  for (var i = 0; i < 25; i++) {
+    var randomx = random(50, 1230);
+    var randomy = random(50, 670);
+    var randomSpeedX = random (-5, 5);
+    var randomSpeedY = random (-5, 5);
+
+    var bal = new Bal(randomx, randomy, randomSpeedX, randomSpeedY);
+
+    ballen.push(bal);
+  }
+
+
+}
 
 
 /**
@@ -32,18 +32,11 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
-  // stel vulkleur in
-  fill(100, 100, 255);
-
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('black');
+  background('blue');
 
- for(var i = 0; i < ballen.length; i++) {
-ballen[i].show
-ballen[i].update();
-
- }
-
-
-
+  for(var i = 0; i < ballen.length; i++) {
+    ballen[i].show();
+    ballen[i].update();
+  }
 }
